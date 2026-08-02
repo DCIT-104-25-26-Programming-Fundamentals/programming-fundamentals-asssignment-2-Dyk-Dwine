1d58d992851c201af8073772b2d913da4504a91c
Dyk-Dwine <dwinekboak06@gmail.com>
2026-08-02 23:48:16 +0000

    Add files via upload

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,33 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    // Check if n is less than 2
+    if (n < 2) {
+        return false;
+    }
+    
+    // Check for factors from 2 to the square root of n
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false; // n is divisible by i, hence not prime
+        }
+    }
+    
+    return true; // n is prime
+}
+
+int main() {
+    int number;
+    
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+    
+    return 0;
+}
